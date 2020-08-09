8ae0b75a0588ffd51fa38321d35cd6aa76a9a0ff
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-08-09 15:36:09 +0530

    Error handling

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
+require("express-async-errors");
+const error = require("./middleware/error");
 const express = require("express");
 const mongoose = require("mongoose");
 const plants = require("./routes/plants");
@@ -8,10 +11,23 @@ const users = require("./routes/users");
 const auth = require("./routes/auth");
 const carts = require("./routes/carts");
 
+process.on("uncaughtException", (ex) => {
+  console.log(ex.message);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (ex) => {
+  console.log("SOMETHING FAILED");
+  process.exit(1);
+});
+
+if (!config.get("jwtPrivateKey")) {
+  throw new Error("jwtPrivateKey is not defined");
+}
+
 mongoose
   .connect("mongodb://localhost:27017/adopt")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+  .then(() => console.log("Connected to MongoDB"));
 
 const app = express();
 app.use(express.json());
@@ -21,6 +37,7 @@ app.use("/api/plants", plants);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/carts", carts);
+app.use(error);
 
 const port = process.env.PORT ? process.env.PORT : 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
